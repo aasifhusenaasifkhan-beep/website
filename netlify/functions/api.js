775a659d81c3bf12a3ff0534a4223848d5719e05
aasifhusenaasifkhan-beep <aasifhusenaasifkhan@gmail.com>
2026-06-27 17:04:28 +0530

    Update api.js

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,4 +1,5 @@
 const serverless = require("serverless-http");
 const app = require("./api/index.js");
-module.exports = app;
+
+// Do not override module.exports with the app object to avoid AWS/Netlify HandlerNotFound errors
 module.exports.handler = serverless(app);
